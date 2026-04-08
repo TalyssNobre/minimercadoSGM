a6770d49cd5d81951365158e16da44155316e69b
Talys Alexandre Nobre <talysalexandrenobre2004@gmail.com>
2026-04-08 16:31:12 -0300

    trocando o middware para proxy

--- minimercado/src/proxy.ts
+++ minimercado/src/proxy.ts
@@ -1,7 +1,8 @@
 import { createServerClient } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from 'next/server';
 
-export async function middleware(request: NextRequest) {
+// 🟢 MUDANÇA AQUI: A função agora se chama "proxy" em vez de "middleware"
+export async function proxy(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request: { headers: request.headers },
   });
